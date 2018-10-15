80db98c2e4a91e2a7c180c1ee95274f3265fe10f
sho <mamemochi1226@gmail.com>
2018-10-15 14:36:09 +0900

    Add files via upload

--- paint2.py
+++ paint2.py
@@ -19,11 +19,11 @@ import pipeline
 parser = argparse.ArgumentParser()
 
 #入出力path設定(カメラの場合使用しない)
-parser.add_argument('--input_image', default='./sample/test.jpg', help='path of input page image')
+parser.add_argument('--input_image', default='./sample/test3.jpg', help='path of input page image')
 parser.add_argument('--output', default='./sample/colorized_test.png',help='the path of colorized image.')
 
 #着色時参照pathの設定
-parser.add_argument('--reference_image', default='./sample/Belmondo-1.png', help='paths of reference images')
+parser.add_argument('--reference_image', default='./sample/HinagikuKenzan-1.png', help='paths of reference images')
 
 #モデルpathの設定
 parser.add_argument('--comicolorizatoin_model_directory', default='./model/comicolorization/',
@@ -35,7 +35,7 @@ parser.add_argument('--super_resolution_model_directory', default='./model/super
 parser.add_argument('--super_resolution_model_iteration', type=int, default=80000,
 					help='the trained model iteration for the super resolution task.')
 #gpuの設定
-parser.add_argument('--gpu', type=int, default=0,help='gpu number (-1 means the cpu mode).')
+parser.add_argument('--gpu', type=int, default=-1,help='gpu number (-1 means the cpu mode).')
 args = parser.parse_args()
 #====================================================================================
 #ニューラルネットワークの設定
@@ -53,88 +53,83 @@ drawer_sr = comicolorization_sr.drawer.Drawer(
 drawer_sr.load_model(iteration=args.super_resolution_model_iteration)
 #====================================================================================
 #カメラの横と縦の長さ
-WIDTH,HEIGHT = 800,600
+WIDTH,HEIGHT = 200,150
+#WIDTH,HEIGHT = 550,411
 #====================================================================================
+input_image = Image.open(args.input_image).convert('RGB')
 #着色時参照画像読み込み
 reference_images = [Image.open(args.reference_image).convert('RGB')]
+
 #枠の設定(元が漫画なので必要だった)
 rect = [[0,0,WIDTH,HEIGHT],]
 #====================================================================================
+
+def BGR2BINARY(image):
+	hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+	h_img, s_img, v_img = cv2.split(hsv)
+	#cv2.THRESH_OTSUをフラグに足すと閾値を自動決定してくれます。
+	_, thresh_img = cv2.threshold(v_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	return thresh_img
+
 #着色処理
-"""
 class DrawThread(threading.Thread):
+	drawn_image = numpy.zeros((HEIGHT,WIDTH,3))
 	def __init__(self, frame):
 		super(DrawThread, self).__init__()
 		self._frame = frame
 
 	def run(self):
-		#グレースケール化
-		self._frame = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
-		#2値化
-		ret,self._frame = cv2.threshold(self._frame,110,255,cv2.THRESH_BINARY)
+		thresh_img = BGR2BINARY(self._frame)
 		#グレースケール化で減った次元を直す
-		self._frame=self._frame.reshape((WIDTH,HEIGHT,1))
-		self._frame = numpy.concatenate([self._frame,self._frame,self._frame],axis=2)
-		#OpenVCからPILに変換
-		self._frame=Image.fromarray(self._frame)
+		thresh_img = cv2.resize(thresh_img,(WIDTH,HEIGHT)).reshape((HEIGHT,WIDTH,1))
+		thresh_img = numpy.concatenate([thresh_img,thresh_img,thresh_img],axis=2)
+		#OpenCVからPILに変換
+		thresh_img = Image.fromarray(thresh_img)
+
 		#各変数設定
 		pipelines = pipeline.PagePipeline( \
 			drawer=drawer, \
 			drawer_sr=drawer_sr, \
-			image=self._frame, \
+			image=thresh_img, \
+			#image = input_image, \
 			reference_images=reference_images, \
-			threshold_binary=190, \
-			threshold_line=130, \
+			#threshold_binary=190, \
+			threshold_binary=100, \
+			#threshold_line=130, \
+			threshold_line=80, \
 			panel_rects=rect, \
 		)
-		#着色
-		print("draw_start :")
-		start = time.time()
-		#drawn_image = pipelines.process()
-		print("time >> ",time.time()-start)
 
-"""
-def Draw_img(_frame):
-	_width = _frame.shape[0]
-	_height = _frame.shape[1]
-	#グレースケール化
-	_frame = cv2.cvtColor(_frame,cv2.COLOR_RGB2GRAY)
-	#2値化
-	ret,_frame = cv2.threshold(_frame,110,255,cv2.THRESH_BINARY)
-	#グレースケール化で減った次元を直す
-	_frame=_frame.reshape((_width,_height,1))
-	_frame = numpy.concatenate([_frame,_frame,_frame],axis=2)
-	#OpenVCからPILに変換
-	_frame=Image.fromarray(_frame)
-	#各変数設定
-	pipelines = pipeline.PagePipeline( \
-		drawer=drawer, \
-		drawer_sr=drawer_sr, \
-		image=_frame, \
-		reference_images=reference_images, \
-		threshold_binary=190, \
-		threshold_line=130, \
-		panel_rects=rect, \
-	)
-	#着色
-	start = time.time()
-	print("draw start")
-	drawn_image = pipelines.process()
-	print("draw end. time >> ",time.time()-start)
-	return drawn_image
+		#着色
+		print("start draw >>>")
+		start = time.time()
+		DrawThread.drawn_image = pipelines.process()
+		DrawThread.drawn_image.save("test.png")
+		#PILからOpenCVに変換
+		DrawThread.drawn_image = numpy.asarray(DrawThread.drawn_image)[:,:,::-1]
+		
+		print("time >> ",time.time()-start)
+		print("finish draw ...")
 
 
 if __name__=="__main__":
 	cap = cv2.VideoCapture(1)
-	cap.set(3,WIDTH) #width
-	cap.set(4,HEIGHT) #height
-
+	cap.set(3,400) #width
+	cap.set(4,300) #height
+	
 	ret, frame = cap.read()
+	drawn_image_tmp = numpy.zeros((HEIGHT,WIDTH,3))
 	while cap.isOpened():
 		#キャプチャ
 		ret, frame = cap.read()
-		Draw_img(frame)
-		cv2.imshow('camera capture', frame)
+		
+		if(threading.activeCount() == 1):
+			drawn_image_tmp = DrawThread.drawn_image
+			th = DrawThread(frame)
+			th.start()
+		
+		cv2.imshow('drawn_image', drawn_image_tmp)
+		cv2.imshow("camera",BGR2BINARY(frame))
 		k = cv2.waitKey(10) # 1msec待つ
 		if k == 27: # ESCキーで終了
 			break
